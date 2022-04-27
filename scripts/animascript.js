2d5ee9dadf87dea696219816dd25f3924ebb71f8
Carolina Dias <carolinatdias@live.com>
2022-04-27 19:48:02 -0300

    adicionado eventos de animacao com js

---
+++ scripts/animascript.js
@@ -0,0 +1,6 @@
+function displayAbout(){
+    var arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('about-me');
+    display.classList.toggle('active');
+}
